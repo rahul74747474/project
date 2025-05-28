57ebf9f26b87099044c765fe65c6840a7719daf0
rahul74747474 <g45238948@gmail.com>
2025-05-28 19:27:22 +0530

    Update CourseCreate.jsx

--- frontend/src/admin/CourseCreate.jsx
+++ frontend/src/admin/CourseCreate.jsx
@@ -16,7 +16,7 @@ function CourseCreate() {
     e.preventDefault();
     console.log("1 step1")
 
-    const admin = JSON.parse(localStorage.getItem("admin"));
+    const admin = JSON.parse(localStorage.getItem("adminToken"));
     const token = admin?.token;
      console.log("Token from localStorage:", token);
 
